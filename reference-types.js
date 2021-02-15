bad83be1bb9561ee869fb55e2b4773842bef0050
Patrick Cool <patrickscool@gmail.com>
2021-02-14 19:40:55 -0800

    Date stuff

--- reference-types.js
+++ reference-types.js
@@ -27,3 +27,24 @@ var blankArray = [, , ,];
 console.log(blankArray.length)//3 in most browsers except 4 in older versions of IE
 entries[42] = 'Shacool';
 console.log(entries.length);//43 assigning a value to 42 created all the entries in between as blank
+
+var ratings = [3, 5, 7];
+console.log(ratings.join('|'));
+
+ratings.push(5);
+ratings.push(2, 4);
+var firstRating = ratings.shift();
+console.log(ratings.length);
+console.log(firstRating);
+var newRatings = ratings.concat([4, 5, 6]);
+console.log(newRatings.length);
+var slicedArray = newRatings.slice(1); //takes first index
+slicedArray = newRatings.slice(2, 3); //takes between index 2 and 3
+slicedArray = newRatings.slice(-1); //moves back one
+var splicedArray = newRatings.splice(1);//
+var sortedArray = [10, 9, 7, 1, 4, 3, 2];
+console.log(sortedArray.sort()); //sorts by string element [1, 10, 2, 3, 4, 7, 9]
+
+var dt = new Date('Patrick'); //will return invalid date
+var realDate = new Date('02/14/2021');
+var stringDate = new Date('July 7 2021');
