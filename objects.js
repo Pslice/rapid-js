8962f27a2005473b9562ca521d4a4b7aa5f2e67b
Patrick Cool <patrickscool@gmail.com>
2021-02-15 21:08:56 -0800

    Moar objects

--- objects.js
+++ objects.js
@@ -9,3 +9,26 @@ project.updateDue = function () {
 console.log(project['securityLevel']); //bracket notation
 var securityField = 'securityLevel';
 console.log(project[securityField]);//using a variable to show notation
+
+var probject = {
+    name: 'Emperor',
+    securityLevel: 2,
+    due: function () {
+        return true;
+    },
+    team: ['Hol Horse', 'Patrick', 'Jotaro'];
+};
+console.log(probject.name);
+console.log(probject.team[1]);
+
+var tasker = {
+    tasks: [
+        {
+            name: 'task 1'
+        },
+        {
+            name: 'task 2'
+        }
+    ]
+}
+console.log(tasker.tasks[1].name);
