2f77752ea742a8e6f08b117898dcbdb7609ecbd8
Patrick Cool <patrickscool@gmail.com>
2021-02-17 22:23:35 -0800

    raise function

--- functions.js
+++ functions.js
@@ -1,6 +1,9 @@
 var Employee = function (name, boss) {
     this.name = name;
     this.boss = boss;
+    this.giveRaise = function (money) {
+        return money++;
+    }
 };
 var newEmployee = new Employee('PC');
 
