43d542bc57d60b76f062571ee3fb956bacf6323c
Patrick Cool <patrickscool@gmail.com>
2021-02-18 21:27:26 -0800

    this now window

--- this.js
+++ this.js
@@ -4,5 +4,13 @@ var employee = {
     name: 'Jeff',
     updateSalary: function () {
         console.log(this);
+    },
+    newSalary: function () {
+        var fn = function () {
+            console.log(this);
+        };
+        fn();
     }
-}
+};
+employee.updateSalary();//Object {name: 'Jeff'}
+employee.newSalary();//window is this (logged for browsers)
