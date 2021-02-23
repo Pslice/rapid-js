4bfa3ea486476390a1323478941f83dfe0420036
Patrick Cool <patrickscool@gmail.com>
2021-02-22 21:39:31 -0800

    added timer function

--- bom.js
+++ bom.js
@@ -1,4 +1,9 @@
 //Browser Object Model
 //window
 console.log(window.innerWidth + ',' + window.outerWidth);
-window.open('https://www.foxthermal.com');
+window.open('https://www.foxthermal.com', '_blank');
+
+console.log(new Date().getSeconds());
+setTimeout(function () {
+    console.log(new Date().getSeconds());
+}, 1000);
