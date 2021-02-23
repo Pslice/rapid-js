5fd691c6cb9a47cead4342be8953447fa74b7bda
Patrick Cool <patrickscool@gmail.com>
2021-02-22 21:42:01 -0800

    id variable

--- bom.js
+++ bom.js
@@ -4,7 +4,12 @@ console.log(window.innerWidth + ',' + window.outerWidth);
 window.open('https://www.foxthermal.com', '_blank');
 
 console.log(new Date().getSeconds());
-setTimeout(function () {
+var id = setTimeout(function () {
     console.log(new Date().getSeconds());
 }, 1000);
 clearTimeout(id);
+
+var interval = setInterval(function () {
+    var secs = new Date().getSeconds();
+    console.log(secs)
+})
