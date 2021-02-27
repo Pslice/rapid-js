9d899371db9155e26846688c19256137a1e694d2
Patrick Cool <patrickscool@gmail.com>
2021-02-26 21:43:03 -0800

    npm package

--- create.sh
+++ create.sh
@@ -1,4 +1,29 @@
 #!/bin/bash
 echo "Please select style:"
+echo "1 - Bootstrap"
+echo "2 - Bulma"
+echo "3 - Tailwind"
 read style
-echo style + " chosen"
+case $style in
+    1)
+    echo "The OG Boot"
+    ;;
+    2)
+    echo "DBZ fan huh?"
+    ;;
+    3)
+    echo "Tailwinder"
+    ;;
+    *)
+    echo "I didn't get that one"
+    ;;
+esac
+function Bootsrap {
+
+}
+function Bulma {
+
+}
+function Tailwind {
+
+}
