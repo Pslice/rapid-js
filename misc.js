6627758ac9bd9dc0489a2aa93a5fefad9c85366e
Patrick Cool <patrickscool@gmail.com>
2021-02-26 23:13:40 -0800

    encode decode

--- misc.js
+++ misc.js
@@ -16,3 +16,9 @@ isNaN(value) //flase Infinity is considered a number
 var path = "\\start\\";
 console.log(encodeURI(path));//%5Cstart%5C
 
+var path = "\\start+";
+console.log(encodeURI(path));//%5Cstart+
+
+console.log(encodeURIComponent(path));//%5Cstart%2B
+path = "%5Cstart%2B";
+console.log(decodeURI(path));//\\start+
