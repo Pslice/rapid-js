41d26aaa8c02fe315ec02d75680368beea45306c
Patrick Cool <patrickscool@gmail.com>
2021-02-27 22:04:18 -0800

    splice and dice

--- strings.js
+++ strings.js
@@ -5,3 +5,6 @@ console.log(value.concat(' yes!'));
 console.log(value.constains("in")); //true
 console.log(value.endswith('gs'));
 console.info(value.lastIndexOF('g'));
+console.log(value.slice(3));
+console.info(value.substr(2, 3));
+console.log(value.substring(5, 6));
