01b0bb3b676d05ced8b06353ca81c5d69651b60b
Patrick Cool <patrickscool@gmail.com>
2021-02-27 22:01:34 -0800

    indices

---
+++ strings.js
@@ -0,0 +1,7 @@
+var value = 'strings';
+console.info(value.charAt(3));
+
+console.log(value.concat(' yes!'));
+console.log(value.constains("in")); //true
+console.log(value.endswith('gs'));
+console.info(value.lastIndexOF('g'));
