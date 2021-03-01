0dc85927022d4cedf92ae4b7ee9b1953d1a686b4
Patrick Cool <patrickscool@gmail.com>
2021-02-28 21:28:47 -0800

    moar finally

--- error-handling.js
+++ error-handling.js
@@ -1,8 +1,8 @@
 //promise more than try catch
 try {
-
-} catch (error) {
-
+    throw new Error('custom error');
+} catch (e) {
+    console.log(e.message);
 } finally {
-
+    console.log('final');
 }
