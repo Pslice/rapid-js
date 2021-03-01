70f3f51d3aad310e2367ba357898f8144389aa9d
Patrick Cool <patrickscool@gmail.com>
2021-02-28 21:31:32 -0800

    range err

--- error-handling.js
+++ error-handling.js
@@ -3,6 +3,9 @@ try {
     throw new Error('custom error');
 } catch (e) {
     console.log(e.message);
+    if (e instanceof RangeError) {
+        console.info('was a range')
+    }
 } finally {
     console.log('final');
 }
